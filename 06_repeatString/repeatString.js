12240f169849e9054b23e6994dbcb24b175b0c54
trisha-c-a <trishaviking@gmail.com>
2025-08-21 17:33:15 -0500

    Finish exercise 06

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(word, num) {
+    let result = "";
+    if (num<0){
+        return "ERROR";
+    }
+    else{
+    for(i=0;i<num;i++){
+        result += word;
+    }
 
+    return result;
+}
 };
 
 // Do not edit below this line
