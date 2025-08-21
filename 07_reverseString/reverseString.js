c8b9f60ec835ebeb4e2ff50c92d86cfe71360937
trisha-c-a <trishaviking@gmail.com>
2025-08-21 17:36:38 -0500

    Finish exercise 07

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(phrase) {
+    result = ""
+    for (i = phrase.length-1; i>-1;i--){
+        result+=phrase[i]
+    }
 
+    return result
 };
 
 // Do not edit below this line
