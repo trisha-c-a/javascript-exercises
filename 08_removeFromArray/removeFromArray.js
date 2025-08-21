83d8f04d20d8ecaf6273e3fe2a80a780432ffa9a
trisha-c-a <trishaviking@gmail.com>
2025-08-21 18:02:02 -0500

    Finish exercise 08

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...values) {
+    const result = [];
+    const set = new Set(values);
+
+    for(i=0;i<array.length;i++){
+        if (!set.has(array[i])) {
+            result.push(array[i]);
+        }
+    }
+    
+    return result
 };
 
 // Do not edit below this line
