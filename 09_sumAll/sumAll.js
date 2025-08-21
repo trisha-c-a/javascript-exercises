ed8f875ec56419973e870aa2fd494dd2218f4a1f
trisha-c-a <trishaviking@gmail.com>
2025-08-21 18:09:04 -0500

    Finish exercise 09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+    if(!Number.isInteger(a) || !Number.isInteger(b) || a<0 || b<0){
+        return "ERROR"
+    }
+    else{
+        let result = 0;
+        if(b>a){
+        for(i=a;i<b+1;i++){
+            result+=i;
+        }
+    }
+    else{
+        for(i=b;i<a+1;i++){
+            result+=i;
+        }
+    }
+        return result
+    }
+    
 };
 
 // Do not edit below this line
