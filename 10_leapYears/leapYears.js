07066466c92df4d6dc29b1d4fabc12970ac57611
trisha-c-a <trishaviking@gmail.com>
2025-08-24 13:14:57 -0500

    Finish exercise 10

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,24 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+//Year is divisible by 100
+//Check if year is divisible by 400 -> If yes, leap else no leeap
+//Not divisible by 100
+//Check if divisble by 4 ->If yes, leap, else no leap
+if (year%100==0){
+    if (year%400==0){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+else{
+    if (year%4==0){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
 };
 
 // Do not edit below this line
