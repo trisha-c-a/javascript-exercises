877d7ca9c29c73c73250f74af2e5e2152c6dce74
trisha-c-a <trishaviking@gmail.com>
2025-08-24 13:22:54 -0500

    Finish exercise 11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farenheitTemp) {
+  celsiusTemp = (farenheitTemp-32)*(5/9);
+  return Math.round(celsiusTemp*10)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusTemp) {
+  farenheitTemp = (celsiusTemp*(9/5))+32;
+  return Math.round(farenheitTemp*10)/10;
 };
 
 // Do not edit below this line
