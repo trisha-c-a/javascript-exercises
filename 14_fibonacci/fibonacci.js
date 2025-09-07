a920c399b356227583dc33b6b2efcebe5a8a3217
trisha-c-a <trishaviking@gmail.com>
2025-09-07 13:17:01 -0500

    Finish 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(length) {
+    if(length<0){
+        return "OOPS";
+    }
+    
+    let num1 = 0;
+    let num2 = 1;
+    for(i=0;i<Number(length);i++)
+    {
+        temp = num2;
+        num2 = num1+num2;
+        num1 = temp;
 
+    }
+    return num1;
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
+
+// fibonacci(4)
