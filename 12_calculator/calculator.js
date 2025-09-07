3ab1ef44bac13aa347b14d6dac76237f64f35f3a
trisha-c-a <trishaviking@gmail.com>
2025-09-07 12:43:36 -0500

    Finish 12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(n1, n2) {
+	return n1+n2;
 };
 
-const subtract = function() {
-	
+const subtract = function(n1,n2) {
+	return n1 - n2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let result = 0;
+  for(i=0;i<arr.length;i++){
+    result+=arr[i];
+  }
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let result = 1;
+  for(i=0;i<arr.length;i++){
+    result*=arr[i];
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function(num,pow) {
+	let result = 1;
+  for(i=0;i<pow;i++){
+    result*=num;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let result = 1;
+  for(i=num;i>0;i--){
+    result*=i;
+  }
+  return result;
 };
 
 // Do not edit below this line
