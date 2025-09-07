f960d3cabe1f71551e7a105fc699b2c9db1f3e40
trisha-c-a <trishaviking@gmail.com>
2025-09-07 12:56:20 -0500

    Finish 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,4 +1,25 @@
-const palindromes = function () {
+const palindromes = function (phrase) {
+    //Remove punctuation
+    //lower case phrase
+    const punctuation = new Set([
+                                '.', ',', '!', '?', ':', ';', '-', '—',
+                                '(', ')', '[', ']', '{', '}', '"', "'", '…', " "
+                                ]);
+    let newPhrase = "";
+    for (i=0;i<phrase.length;i++){
+        if (!punctuation.has(phrase[i])){
+            newPhrase+=phrase[i].toLowerCase();
+        }
+    }
+    
+    const newPhraseLength = newPhrase.length;
+
+    for(i=0;i<newPhraseLength/2;i++){
+        if(newPhrase[i]!=newPhrase[newPhraseLength-i-1]){
+            return false;
+        }
+    }
+    return true;
 
 };
 
