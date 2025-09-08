95871ecdde72ea0667a8da31c2cad078f4313237
trisha-c-a <trishaviking@gmail.com>
2025-09-07 20:34:57 -0500

    Finish 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let result = {};
+    let age = 0
+    for (person of people){
+        if (person.yearOfDeath){
+            if(person.yearOfDeath-person.yearOfBirth>age){
+                age = person.yearOfDeath-person.yearOfBirth;
+                result = person;
+            }
+        }
+        else{
+            const year = new Date().getFullYear();
+            if(year-person.yearOfBirth>age){
+                age = year-person.yearOfBirth;
+                result = person;
+            }
+        }
+    }
 
+    return result;
 };
 
 // Do not edit below this line
